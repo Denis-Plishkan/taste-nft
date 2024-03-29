5299102a47b82a9ce6fa5325f41445b8d0bb71b2
Denis-Plishkan <frontend.plishkan@gmail.com>
2024-03-29 17:00:16 +0200

    Updates

--- assets/ArtworkPage-DvFwof8J.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as r}from"./index-DyJxhNm8.js";const o={};function t(n,a){return r(),c("h1",null,"Одна работа")}const _=e(o,[["render",t]]);export{_ as default};
--- assets/MainPage-CK0IlMWp.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o,a as s}from"./index-DyJxhNm8.js";const t={},n={class:"container"},a=s("h1",null,"Главная",-1),_=[a];function r(i,d){return o(),c("div",n,_)}const f=e(t,[["render",r]]);export{f as default};
