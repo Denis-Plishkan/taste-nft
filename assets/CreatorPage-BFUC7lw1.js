ce4b73685f3b887b99e222ee74a331b468976ed9
Denis-Plishkan <frontend.plishkan@gmail.com>
2024-04-18 17:17:20 +0300

    Updates

--- assets/CreatorPage-BFUC7lw1.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as r}from"./index-D1P72Gcc.js";const o={};function t(n,a){return r(),c("h1",null,"Создание")}const _=e(o,[["render",t]]);export{_ as default};
---
+++ assets/ProfilePage-B-YL_GOv.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./index-B7Qth3mk.js";const r={};function n(t,a){return o(),c("h1",null,"Профиль")}const _=e(r,[["render",n]]);export{_ as default};
